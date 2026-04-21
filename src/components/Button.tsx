3aa986e5a7e16426a7db728f1fea1db09758bf5d
small-lizard <a.kulabukhova001@gmail.com>
2026-04-21 16:57:20 +0400

    feat: Initial project setup with fonts, icons, folder structure and base component

---
+++ src/components/Button.tsx
@@ -0,0 +1,42 @@
+type ButtonVariant = "primary" | "secondary";
+
+type ButtonProps = {
+  text: string;
+  variant?: ButtonVariant;
+  withIcon?: boolean;
+  onClick?: () => void;
+  className?: string;
+};
+
+const Button: React.FC<ButtonProps> = ({
+  text,
+  variant = "primary",
+  withIcon = false,
+  onClick,
+  className = "",
+}) => {
+  const baseStyles =
+    "inline-flex items-center justify-center font-dmsans cursor-pointer uppercase rounded-[10px]";
+
+  const variants = {
+    primary:
+      "h-[50px] px-[26px] gap-[15px] font-bold text-[16px] bg-primary text-text",
+    secondary:
+      "h-[40px] px-[20px] font-medium text-[14px] bg-dark text-text",
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`${baseStyles} ${variants[variant]} ${className}`}
+    >
+      <span>{text}</span>
+      {withIcon && variant === "primary" && (
+        <img src="/src/assets/icons/green-arrow.svg" alt="Arrow icon" className="w-[27px] h-[27px]" />
+      )}
+    </button>
+  );
+};
+
+export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,27 @@
 export default {
- content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
- theme: {
-   extend: {
-     fontFamily: {
-       delagothicone: ['DelaGothicOne', 'sans-serif'],
-       dmsans: ['DM Sans', 'sans-serif'],
-     },
-   },
- },
- plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        delagothicone: ['DelaGothicOne', 'sans-serif'],
+        dmsans: ['DM Sans', 'sans-serif'],
+      },
+      colors: {
+        primary: "#8F5CFB",
+        accent: "#96F21B",
+        dark: "#403E3E",
+        text: {
+          DEFAULT: "#EEEEEE",
+          secondary: "#6F6B6B",
+          accent: "#B7FF56",
+        },
+        background: {
+          DEFAULT: "#201F1F",
+        },
+      },
+    },
+  },
+  plugins: [],
 }
 
 
